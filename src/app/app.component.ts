b58c55ee49d060dd4ec29763d2502e8eb994dac4
Charles de La Rochefoucauld <charlesdelr@gmail.com>
2018-10-01 10:25:51 +0200

    blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  title = 'Blog';
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      loveIts: 3,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      loveIts: -2,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon dernier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
+
 }
